0d63d4c9f0fff3db628f2a7331e1b708c266c969
Roberto <mrrobertrausch@gmail.com>
2022-12-21 06:15:51 +0000

    removed unecessary globals 	modified:   fluid.h

--- fluid.h
+++ fluid.h
@@ -10,9 +10,6 @@ fluid_synth_t* synth;
 fluid_settings_t* settings;
 fluid_audio_driver_t* adriver;
 
-vector<int> blackNotes = {36,38,41,43,45,48,50,53,55,57,60,62,65,67,69,72,74,77,79,81};
-vector<int> whiteNotes = {35,37,39,40,42,44,46,47,49,51,52,54,56,58,59,61,63,64,66,68,70,71,73,75,76,78,80,82,83};
-
 void startSynth(){
 	settings = new_fluid_settings();
 	synth = new_fluid_synth(settings);
@@ -21,7 +18,8 @@ void startSynth(){
 }
 
 void whiteKey::playWhiteNote(int n){
-	fluid_synth_noteon(synth,0,::whiteNotes[n],100);
+	vector<int> whiteNotes = {35,37,39,40,42,44,46,47,49,51,52,54,56,58,59,61,63,64,66,68,70,71,73,75,76,78,80,82,83};
+	fluid_synth_noteon(synth,0,whiteNotes[n],100);
 }
 
 void whiteKey::releaseNote(){
@@ -29,7 +27,8 @@ void whiteKey::releaseNote(){
 }
 
 void blackKey::playBlackNote(int n){
-	fluid_synth_noteon(synth,0,::blackNotes[n],100);
+	vector<int> blackNotes = {36,38,41,43,45,48,50,53,55,57,60,62,65,67,69,72,74,77,79,81};
+	fluid_synth_noteon(synth,0,blackNotes[n],100);
 }
 
 void blackKey::releaseNote(){
